4506446659c9b496a9d875e0862f62dca3de497c
Celeste Lewis <cdlewis42@gmail.com>
2020-03-19 13:15:57 -0400

    Server is working

---
+++ server.js
@@ -0,0 +1,15 @@
+//requiring express and path
+var express = require("express")
+var path = require ("path")
+
+var app = express()
+var PORT = process.env.PORT || 8080
+
+//Middle ware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(PORT, function() {
+    console.log("App listening on http://localhost:" + PORT);
+  });
